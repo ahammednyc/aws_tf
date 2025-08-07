294d72e4bed61cb36e7b49834719eec34a38e3e1
Alamgir <ahammednyc@gmail.com>
2025-08-07 11:25:13 -0400

    updared github actions

--- terrafrom_modules/dev/main.tf
+++ terrafrom_modules/dev/main.tf
@@ -1,12 +1,22 @@
 module "sampl" {
   source        = "../modules"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   ec2_tag       = "HelloWorld"
 }
 
 
 module "samp2" {
   source        = "../modules"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   ec2_tag       = "test"
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-521409"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
